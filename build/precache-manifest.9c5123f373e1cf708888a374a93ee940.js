f564fa606b6586664eaad3e4f1e6bdd94578c77f
Bohdan Koldun <38987744+bohdan-koldun@users.noreply.github.com>
2018-12-18 04:15:39 +0200

    add build 1;

---
+++ build/precache-manifest.9c5123f373e1cf708888a374a93ee940.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ce9e417a577bd34321bfc3d8788f258",
+    "url": "/static/media/dog4.2ce9e417.jpg"
+  },
+  {
+    "revision": "dcdd9149e72bee0b19d4a6a2debb70d1",
+    "url": "/static/media/cat3.dcdd9149.jpg"
+  },
+  {
+    "revision": "ba3540085227e18609af43ba762d7732",
+    "url": "/static/media/cat2.ba354008.jpg"
+  },
+  {
+    "revision": "1bf0ea9ad9527492f07e96c7ae2d894c",
+    "url": "/static/media/cat1.1bf0ea9a.jpg"
+  },
+  {
+    "revision": "57c3b63d35efced128edc244920fea03",
+    "url": "/static/media/arrow-down.57c3b63d.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84a0065afd5e9c5bd372",
+    "url": "/static/js/main.84a0065a.chunk.js"
+  },
+  {
+    "revision": "173ca59a7ab41c5c181e",
+    "url": "/static/js/1.173ca59a.chunk.js"
+  },
+  {
+    "revision": "84a0065afd5e9c5bd372",
+    "url": "/static/css/main.5246e17e.chunk.css"
+  },
+  {
+    "revision": "b70fe9ef072fa797fc018851a34c7ac6",
+    "url": "/index.html"
+  }
+];
